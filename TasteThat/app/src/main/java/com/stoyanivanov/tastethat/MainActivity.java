c4678f728d0b5378d77f7c79814889bc6a0c99c9
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-10-05 01:04:17 +0300

    Added bottom navigation menu

--- TasteThat/app/src/main/java/com/stoyanivanov/tastethat/MainActivity.java
+++ TasteThat/app/src/main/java/com/stoyanivanov/tastethat/MainActivity.java
@@ -1,17 +1,31 @@
 package com.stoyanivanov.tastethat;
 
+import android.app.Application;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.google.firebase.auth.FirebaseAuth;
+import com.stoyanivanov.tastethat.view_utils.BottomNavigationViewHelper;
+
+import java.lang.reflect.Field;
+
+import static com.stoyanivanov.tastethat.R.string.bottomnav_add;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,6 +62,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         replaceFragment(new CombinationsFragment());
+
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(MenuItem item) {
+//                        switch (item.getItemId()) {
+//                            case R.id.nav_button_add:
+//                                replaceFragment(); break;
+
+//                            case R.id.nav_button_home:
+//                                replaceFragment(); break;
+
+//                            case R.id.nav_button_profile:
+//                                replaceFragment(); break;
+
+        //                     case R.id.nav_button_menu:
+        //                          replaceFragment; break;
+//                        }
+//                        return true;
+//                    }
+//                });
 
     }
 
