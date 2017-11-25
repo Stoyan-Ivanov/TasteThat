95c38265093cd2558e2e5b3b005ceaf09a204cdc
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-11-25 14:33:21 +0200

    Added upload achivements cloud function and fixed database structuring

--- TasteThat/functions/index.js
+++ TasteThat/functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+/* 
+	name: profileInit
+	description: creates a user basic information node when a new user is authenticated
+
+*/
 exports.profileInit = functions.auth.user().onCreate(event => {
   	
 	const user = event.data;
@@ -14,13 +18,19 @@ exports.profileInit = functions.auth.user().onCreate(event => {
 	admin.database().ref('users/' + user.uid + '/userInfo').set(userInfo);
 });
 
+
+/*
+	name: likeAchievementsChecker
+	description: function that checks if the user is able to obtain a new like achievement.
+		If he does the function adds the achievement to the user's achievements node.
+*/
 exports.likeAchievementsChecker = functions.database
 .ref('users/{uid}/likedCombinations').onWrite(event => {
 	
 	const uid = event.params.uid;
 	const numberOfLikes = event.data.numChildren();
 	
-	const getAchievementsPromise = admin.database().ref('likeAchievements')
+	const getAchievementsPromise = admin.database().ref('achievements/likeAchievements')
 	return  getAchievementsPromise.once('value').then(snapshot => {
 		snapshot.forEach(achievement => {
 			var parsedAchievement = achievement.val();
@@ -29,7 +39,34 @@ exports.likeAchievementsChecker = functions.database
 				
 				event.data.ref.parent.child('achievements').child(parsedAchievement['name']).set(parsedAchievement);
 			} else {
-				console.log("not enough likes for next achievement");
+				//console.log("not enough likes for next achievement");
+			}
+		});
+	});
+});
+
+
+/*
+	name: uploadAchievementsChecker
+	description: function that checks if the user is able to obtain a new upload achievement.
+		If he does the function adds the achievement to the user's achievements node.
+*/
+exports.uploadAchievementsChecker = functions.database
+.ref('users/{uid}/uploadedCombinations').onWrite(event => {
+	
+	const uid = event.params.uid;
+	const numberOfUploads = event.data.numChildren();
+	
+	const getAchievementsPromise = admin.database().ref('achievements/uploadAchievements')
+	return  getAchievementsPromise.once('value').then(snapshot => {
+		snapshot.forEach(achievement => {
+			var parsedAchievement = achievement.val();
+
+			if(numberOfUploads >= parsedAchievement['value']) {
+				
+				event.data.ref.parent.child('achievements').child(parsedAchievement['name']).set(parsedAchievement);
+			} else {
+				//console.log("not enough likes for next achievement");
 			}
 		});
 	});
