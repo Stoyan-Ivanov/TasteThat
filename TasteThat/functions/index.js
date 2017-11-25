ca4640ecb79003223696f8d90b9003a272edd450
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-11-25 13:40:18 +0200

    Added cloud function for like achievements

--- TasteThat/functions/index.js
+++ TasteThat/functions/index.js
@@ -13,3 +13,24 @@ exports.profileInit = functions.auth.user().onCreate(event => {
 	};
 	admin.database().ref('users/' + user.uid + '/userInfo').set(userInfo);
 });
+
+exports.likeAchievementsChecker = functions.database
+.ref('users/{uid}/likedCombinations').onWrite(event => {
+	
+	const uid = event.params.uid;
+	const numberOfLikes = event.data.numChildren();
+	
+	const getAchievementsPromise = admin.database().ref('likeAchievements')
+	return  getAchievementsPromise.once('value').then(snapshot => {
+		snapshot.forEach(achievement => {
+			var parsedAchievement = achievement.val();
+
+			if(numberOfLikes >= parsedAchievement['value']) {
+				
+				event.data.ref.parent.child('achievements').child(parsedAchievement['name']).set(parsedAchievement);
+			} else {
+				console.log("not enough likes for next achievement");
+			}
+		});
+	});
+});
