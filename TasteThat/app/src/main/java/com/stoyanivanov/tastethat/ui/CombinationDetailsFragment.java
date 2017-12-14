c1ae696eaaeba917366104750245ad63dddd3778
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-12-15 01:14:33 +0200

    Added fragment which will appear on recyclerview item click

---
+++ TasteThat/app/src/main/java/com/stoyanivanov/tastethat/ui/CombinationDetailsFragment.java
@@ -0,0 +1,78 @@
+package com.stoyanivanov.tastethat.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.stoyanivanov.tastethat.R;
+import com.stoyanivanov.tastethat.models.Combination;
+import com.stoyanivanov.tastethat.network.TasteThatApplication;
+import com.stoyanivanov.tastethat.view_utils.CustomTextView;
+
+import java.util.ArrayList;
+
+public class CombinationDetailsFragment extends Fragment {
+    private ImageView imageTopLeft;
+    private ImageView imageTopRight;
+    private ImageView imageBottomLeft;
+    private ImageView imageBottomRight;
+    private CustomTextView combinationNameHeader;
+    private CustomTextView combinationDescription;
+    private Combination currCombination;
+    private ImageView [] images = {imageTopLeft, imageTopRight, imageBottomLeft, imageBottomRight};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_combination_details, container, false);
+
+        combinationNameHeader = (CustomTextView) view.findViewById(R.id.ctv_combination_details_header);
+        imageTopLeft = (ImageView) view.findViewById(R.id.iv_top_left);
+        imageTopRight = (ImageView) view.findViewById(R.id.iv_top_right);
+        imageBottomLeft = (ImageView) view.findViewById(R.id.iv_bottom_left);
+        imageBottomRight = (ImageView) view.findViewById(R.id.iv_bottom_right);
+        combinationDescription = (CustomTextView) view.findViewById(R.id.ctv_combination_details_description);
+
+        currCombination = (Combination) getArguments().getSerializable("currCombination");
+
+        loadCombinationName();
+        loadImages();
+        loadDescription();
+
+        return view;
+    }
+
+    private void loadCombinationName() {
+        StringBuilder displayNameBuilder = new StringBuilder();
+        ArrayList<String> components = currCombination.getComponents();
+
+        for(int i = 0; i < components.size(); i++) {
+            if(i < components.size() - 1) {
+                displayNameBuilder.append(components.get(i));
+                displayNameBuilder.append(" & ");
+            } else {
+                displayNameBuilder.append(components.get(i));
+            }
+        }
+        combinationNameHeader.setText(displayNameBuilder.toString());
+    }
+
+    private void loadImages() {
+        ArrayList<String> urls = currCombination.getUrls();
+
+        for(int i = 0; i < urls.size(); i++) {
+            Glide.with(TasteThatApplication.getStaticContext())
+                    .load(urls.get(i))
+                    .centerCrop()
+                    .into(images[i]);
+        }
+    }
+
+    private void loadDescription() {
+    }
+}
