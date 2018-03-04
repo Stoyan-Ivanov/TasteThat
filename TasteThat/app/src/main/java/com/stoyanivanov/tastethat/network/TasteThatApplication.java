9c6f14977b4055e18b91c4730ce2fe989a78f1ca
Stoyan-Ivanov <stoyaniviv@gmail.com>
2018-03-04 23:17:12 +0200

    Added data caching functionality which enables offline usage of the app

--- TasteThat/app/src/main/java/com/stoyanivanov/tastethat/network/TasteThatApplication.java
+++ TasteThat/app/src/main/java/com/stoyanivanov/tastethat/network/TasteThatApplication.java
@@ -2,10 +2,14 @@ package com.stoyanivanov.tastethat.network;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.Gravity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.stoyanivanov.tastethat.constants.Constants;
 
 /**
  * Created by stoyan-ivanov on 14.11.17.
@@ -18,6 +22,16 @@ public class TasteThatApplication extends Application {
     public void onCreate() {
         super.onCreate();
         applicationContext = getApplicationContext();
+        configureDataCaching();
+    }
+
+    private void configureDataCaching() {
+        final SharedPreferences sharedPrefs = TasteThatApplication
+                .getStaticContext()
+                .getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(
+                sharedPrefs.getBoolean(Constants.CACHING_KEY, false));
     }
 
     public static void showToast(String message) {
@@ -36,7 +50,9 @@ public class TasteThatApplication extends Application {
     }
 
     public static void showVirtualKeyboard() {
-        InputMethodManager imm = (InputMethodManager) applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) applicationContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
     }
 
